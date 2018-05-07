711a5a3f49736c1891590e2f9a497b67eb934847
juloko <julio_flima@hotmail.com>
2018-05-07 18:43:05 -0300

    done without boot

--- js/main.js
+++ js/main.js
@@ -17,6 +17,25 @@ function onSubmit() {
     if (messageInput.value === "") {
         alert("You have to fill the field message.");
         return false;
+    }
 
+    if (validationEmail(emailInput.value)) {
+        console.log("email OK");
+    } else {
+        alert('You have to fill the field email correctly, including and "@" and a"domain.exemple".');
+        return false;
+    }
+}
+
+
+function validationEmail(str) {
+    if (str.search("@") > 0) {
+        var res = str.split("@");
+        resTwo = res[1].split(".");
+        if (res[1] != "" && resTwo[0] != "" && resTwo[1] != "") {
+            console.log("email OK");
+        } else {
+            return false;
+        }
     }
 }
