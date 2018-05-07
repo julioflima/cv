4933990a37bbca814ad979489a8726c044fff71b
juloko <julio_flima@hotmail.com>
2018-05-07 19:24:58 -0300

    add email

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ var nameInput = document.querySelector("#name");
 var emailInput = document.querySelector("#email");
 var messageInput = document.querySelector("#message");
 var submitButton = document.querySelector("#submit");
+var emailTo = "julio_flima@hotmail.com";
+var subjectTo = "CV Git";
+var tokenTo = "ae108ab8-3746-49fb-a1c9-15978486cda4"
+
 
 function onSubmit() {
     if (nameInput.value === "") {
@@ -25,15 +29,28 @@ function onSubmit() {
         alert('You have to fill the field email correctly, including and "@" and a"domain.exemple".');
         return false;
     }
+
+    Email.send(
+        emailInput,
+        emailTo,
+        SubjectTo,
+        messageInput.value, {
+            token: tokenTo,
+            callback: function done(message) {
+                alert("send")
+            }
+        }
+    );
 }
 
 
 function validationEmail(str) {
     if (str.search("@") > 0) {
         var res = str.split("@");
-        resTwo = res[1].split(".");
+        var resTwo = res[1].split(".");
         if (res[1] != "" && resTwo[0] != "" && resTwo[1] != "") {
             console.log("email OK");
+            return true;
         } else {
             return false;
         }
